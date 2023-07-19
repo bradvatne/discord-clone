04de69b1b203966551f4007f29eae4e20d561ee2
brad <brad.vatne@gmail.com>
2023-07-19 11:46:20 +0800

    protected route implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-"use client";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-export const Home = () => {
-  return <div>Hello World</div>;
+export const Home = async () => {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) redirect("/login");
+  return <div>Hello {user.id}</div>;
 };
 
 export default Home;
